9ba312f5a1bf924883519331f68cc4af0dbc8fc8
unknown <jameskagunga2022@gmail.com>
2024-07-06 00:07:55 +0300

    Started Server

---
+++ server/utils/db.ts
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+require("dotenv").config();
+const dbUrl: string = process.env.DB_URI || "";
+const dbConnect = async () => {
+  try {
+    await mongoose.connect(dbUrl).then((data: any) => {
+      console.log(`Database connected with ${data.connection.host}`);
+    });
+  } catch (error: any) {
+    console.log(error.message);
+    setTimeout(dbConnect, 5000);
+  }
+};
+
+export default dbConnect;
